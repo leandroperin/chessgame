5f8b241d1b80701ad37529f1fee5737fc78906bf
Marcos Schead <marcos.m4am@gmail.com>
2014-11-19 00:51:26 -0200

    Revert "#2 TESTE"
    This reverts commit f2dded645e7e8fec9d73a6fa3301d1f51eff2336.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -21,8 +21,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Main {
-
-	//TESTANDO GITHUB
 
 	private JFrame frmChessgame;
 	
